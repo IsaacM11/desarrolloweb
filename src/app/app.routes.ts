f016bb2306106414cdaddae443e1318755d6538a
IsaacM11 <isaacmorgadoabreu@gmail.com>
2025-06-09 17:46:54 -0600

    Subi primer avance de mi app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Products } from './pages/products/products';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:Home},
+    {path:'Nosotros',component:About},
+    {path:'Productos',component:Products},
+    {path:'Contacto',component:Contact},
+    {path:'**',redirectTo:''}
+];
